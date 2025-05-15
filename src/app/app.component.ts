3230139fbd2456c2f2ac60d35a5909d568c1d2ae
Aless4ndr0 <jiwemoyo@gmail.com>
2025-05-15 15:20:07 -0500

    dia 2 y 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SaludoComponent } from "./components/saludo/saludo.component";
 import { PerfilComponent } from "./components/perfil/perfil.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SaludoComponent, PerfilComponent],
+  imports: [RouterOutlet, SaludoComponent, PerfilComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AcercaComponent } from './components/routing/acerca/acerca.component';
+import { ContactoComponent } from './components/routing/contacto/contacto.component';
+import { InicioComponent } from './components/routing/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:InicioComponent},
+  {path:"acerca",component:AcercaComponent},
+  {path:"contacto",component:ContactoComponent}
+];
